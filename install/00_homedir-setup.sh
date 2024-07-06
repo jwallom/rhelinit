25ba16aa4092bb4f7b9c1dc510a2dbbe734b33f9
John Wallom <john@arcs.gatech.edu>
2024-07-06 11:21:08 -0400

    Adds gitconfig to homedir setup

--- install/00_homedir-setup.sh
+++ install/00_homedir-setup.sh
@@ -2,3 +2,11 @@
 
 mkdir -p ~/proojects
 mkdir -p ~/bin
+
+# Basic Git Config
+git_config="./config/gitconfig"
+if [ -f $git_config]; then
+	cp $git_config ~/.gitconfig
+else
+	printf "\n\n\Git config not found.\n\n"
+fi
