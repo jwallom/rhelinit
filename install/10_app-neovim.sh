01fdc002e17ee0686102a2811966046bb8388574
John Wallom <john@arcs.gatech.edu>
2024-07-06 20:45:43 -0400

    Fixes issues 3 and 5.
    Also removes bad idea of coloring messages

--- install/10_app-neovim.sh
+++ install/10_app-neovim.sh
@@ -9,14 +9,16 @@ versha="be1f0988d0de71c375982b87b86cd28d2bab35ece8285abe3b0aac57604dfc5a"
 
 printf "Installing Neovim...\n\n"
 
+sudo dnf install ripgrep -y
+
 mkdir -p ~/projects/neovim
 cd ~/projects/neovim
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 shaval=$(sha256sum nvim-linux64.tar.gz | awk '{print $1}')
 if [ $versha == $shaval ]; then
-	printf "\n${green}${shaval}${normal}\n\n"
+	printf "\n$shaval\n\n"
 else
-	printf "\n${red}$shaval}${normal}\n\n"
+	printf "\n$shaval\n\n"
 fi
 tar -zxf nvim-linux64.tar.gz
 ln -s ~/projects/neovim/nvim-linux64/bin/nvim ~/bin/nvim
--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Setup some colors for output:
-green="\e[;32m"
-red="\e[;31m"
-normal="\e[0m"
-
 #Run stuffs!
 for installer in ./install/*.sh; do source $installer; done
