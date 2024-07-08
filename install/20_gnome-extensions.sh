1b4539ed4ac7c76c68786c97ed619c9eabdcf192
John Wallom <john@arcs.gatech.edu>
2024-07-08 19:55:20 -0400

    Removed pipx install ands checks extensions

--- install/20_gnome-extensions.sh
+++ install/20_gnome-extensions.sh
@@ -13,13 +13,13 @@ printf "Configuring Gnome Extensions...\n\n"
 #gext install tactile@lundal.io
 #gext install just-perfection-desktop@just-perfection
 
-tactile_schema="~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml"
-justper_schema="~/.local/share/gnome-shell/extensions/just-perfection-desktop@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml"
+tactile_schema="$HOME/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml"
+justper_schema="$HOME/.local/share/gnome-shell/extensions/just-perfection-desktop@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml"
 
 if [ -f $tactile_schema ] && [ -f $justper_schema ]; then
 	#Setup schemas
-	sudo cp ~/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas
-	sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas
+	sudo cp $tactile_schema /usr/share/glib-2.0/schemas
+	sudo cp $justper_schema /usr/share/glib-2.0/schemas
 	sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 
 	# Tactile Setup:
@@ -29,7 +29,7 @@ if [ -f $tactile_schema ] && [ -f $justper_schema ]; then
 	gsettings set org.gnome.shell.extensions.tactile col-3 0
 	gsettings set org.gnome.shell.extensions.tactile row-0 1
 	gsettings set org.gnome.shell.extensions.tactile row-1 1
-	gsettings set org.gnome.shell.extensions.tactile gap-size 32
+	gsettings set org.gnome.shell.extensions.tactile gap-size 15
 
 	# Configure Just Perfection
 	gsettings set org.gnome.shell.extensions.just-perfection animation 2
@@ -38,3 +38,4 @@ if [ -f $tactile_schema ] && [ -f $justper_schema ]; then
 	gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
 else
 	printf "\n\nTactile and Just Perfection not installed\nInstall them first\n\n"
+fi
