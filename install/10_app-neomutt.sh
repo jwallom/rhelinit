dac753685d76c3b8fbf243d86d84a3cf21d05865
John Wallom <john@arcs.gatech.edu>
2024-12-24 13:05:32 -0500

    Fix neomutt install to be neomutt and not keychain

--- install/10_app-neomutt.sh
+++ install/10_app-neomutt.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Installs Keychain
+# Installs Neomutt
 
-printf "Insalling keychain..\n\n"
-sudo dnf install keychain -y
+if command -v neomutt &> /dev/null; then
+	printf "\nINFO: Skipping Neomutt installation. Already installed\n\n"
+else
+	printf "Insalling Neomutt..\n\n"
+	sudo dnf install neomutt lynx -y
+fi
