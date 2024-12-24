d5b7b0a06d9b48f803ade733ea8c328b828d6d33
John Wallom <john@arcs.gatech.edu>
2024-12-24 14:24:53 -0500

    Remove keychain in favor of 1Passwork keychain
    No longer using the keychain app to manage ssh keys on RHEL.

--- install/10_app-keychain.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Installs Keychain
-
-if command -v keychain &> /dev/null; then
-	printf "\nINFO: Skipping Keychain install. Already installed.\n\n"
-else
-	printf "Insalling keychain..\n\n"
-	sudo dnf install keychain -y
-fi
