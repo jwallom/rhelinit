d9303c6e54909f1fcee6ec3743935617a8a957f9
John Wallom <john@arcs.gatech.edu>
2024-12-24 13:12:44 -0500

    Add .rhelinit_complete check file and logic

--- install/00_homedir-setup.sh
+++ install/00_homedir-setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-mkdir -p ~/projects
-mkdir -p ~/bin
-
-# Basic Git Config
-git_config="./config/gitconfig"
-if [ -f $git_config ]; then
-	if [ ! -f $HOME/.gitconfig ]; then
-		cp $git_config ~/.gitconfig
-	else
-		printf "\n\nExisting git configuration found\n\n"
-	fi
+if [ -f "$HOME/.rhelinit_complete" ]; then
+	printf "\nINFO: Skipping Home Directory Setup. Remove %s to run configuration\n\n" "$HOME/.rhelinit_complete"
 else
-	printf "\n\n\Git config not found.\n\n"
+	mkdir -p ~/projects
+	mkdir -p ~/bin
+
+	# Basic Git Config
+	git_config="./config/gitconfig"
+	if [ -f $git_config ]; then
+		if [ ! -f $HOME/.gitconfig ]; then
+			cp $git_config ~/.gitconfig
+		else
+			printf "\n\nExisting git configuration found\n\n"
+		fi
+	else
+		printf "\n\n\Git config not found.\n\n"
+	fi
 fi
--- install/20_gnome-extensions.sh
+++ install/20_gnome-extensions.sh
@@ -3,31 +3,35 @@
 # tactile
 # just-perfection-desktop
 
-printf "Configuring Gnome Extensions...\n\n"
-
-tactile_schema="$HOME/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml"
-justper_schema="$HOME/.local/share/gnome-shell/extensions/just-perfection-desktop@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml"
-
-if [ -f $tactile_schema ] && [ -f $justper_schema ]; then
-	#Setup schemas
-	sudo cp $tactile_schema $HOME/.local/share/glib-2.0/schemas
-	sudo cp $justper_schema $HOME/.local/share/glib-2.0/schemas
-	glib-compile-schemas $HOME/.local/share/glib-2.0/schemas/
-
-	# Tactile Setup:
-	gsettings set org.gnome.shell.extensions.tactile col-0 1
-	gsettings set org.gnome.shell.extensions.tactile col-1 2
-	gsettings set org.gnome.shell.extensions.tactile col-2 1
-	gsettings set org.gnome.shell.extensions.tactile col-3 0
-	gsettings set org.gnome.shell.extensions.tactile row-0 1
-	gsettings set org.gnome.shell.extensions.tactile row-1 1
-	gsettings set org.gnome.shell.extensions.tactile gap-size 15
-
-	# Configure Just Perfection
-	gsettings set org.gnome.shell.extensions.just-perfection animation 2
-	gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
-	gsettings set org.gnome.shell.extensions.just-perfection workspace true
-	gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+if [ -f "$HOME/.rhelinit_complete " ]; then
+	printf "\nINFO: Skipping Gnome Extensions. Remove %s to run configuration\n\n" "$HOME/.rhelinit_complete"
 else
-	printf "\n\nTactile and Just Perfection not installed\nInstall them first\n\n"
+	printf "Configuring Gnome Extensions...\n\n"
+
+	tactile_schema="$HOME/.local/share/gnome-shell/extensions/tactile@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml"
+	justper_schema="$HOME/.local/share/gnome-shell/extensions/just-perfection-desktop@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml"
+
+	if [ -f "$tactile_schema" ] && [ -f "$justper_schema" ]; then
+		#Setup schemas
+		cp "$tactile_schema" "$HOME/.local/share/glib-2.0/schemas"
+		cp "$justper_schema" "$HOME/.local/share/glib-2.0/schemas"
+		glib-compile-schemas "$HOME/.local/share/glib-2.0/schemas/"
+
+		# Tactile Setup:
+		gsettings set org.gnome.shell.extensions.tactile col-0 1
+		gsettings set org.gnome.shell.extensions.tactile col-1 2
+		gsettings set org.gnome.shell.extensions.tactile col-2 1
+		gsettings set org.gnome.shell.extensions.tactile col-3 0
+		gsettings set org.gnome.shell.extensions.tactile row-0 1
+		gsettings set org.gnome.shell.extensions.tactile row-1 1
+		gsettings set org.gnome.shell.extensions.tactile gap-size 15
+
+		# Configure Just Perfection
+		gsettings set org.gnome.shell.extensions.just-perfection animation 2
+		gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
+		gsettings set org.gnome.shell.extensions.just-perfection workspace true
+		gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+	else
+		printf "\n\nTactile and Just Perfection not installed\nInstall them first\n\n"
+	fi
 fi
