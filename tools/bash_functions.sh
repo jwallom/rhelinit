b400e1bb5c2a8a1a277abd0dd8208ceb413e08f5
John Wallom <john@arcs.gatech.edu>
2024-12-30 20:59:30 -0500

    Move bash functions to single file. Clean up setup.sh
    Move functions in setup.sh to bash_functions.sh
    
    Doc:Update comments to make setup.sh more clear

--- tools/bash_functions.sh
+++ tools/bash_functions.sh
@@ -76,5 +76,44 @@ install_software() {
 
 }
 
+function continue_prompt {
+    printf "Do you want to continue? (y/n)\n"
+    read -r response
+
+    response=${response,,}
+
+    if [[ "$response" =~ ^(yes|y)$ ]]; then
+        printf "\n\nContinuing...\n"
+        return 0
+    else
+        return 1
+    fi
+}
+
+function is_rhel {
+    local distro
+    local version
+
+    distro="$(grep -oP '(?<=^NAME=").*(?=")' /etc/os-release)"
+    version="$(grep -oP '(?<=^VERSION_ID=").*(?=")' /etc/os-release)"
+
+    if [ "$distro" == "Red Hat Enterprise Linux" ]; then
+        if [[ "$version" =~ ^[0-9]+(\.[0-9]+)*$ ]] && (( $(echo "$version >= 9.4" | bc -l) )); then
+            return 0
+        else
+            printf "WARNING:This should be used on RHEL 9.4+. Current version: %s\n" "$version"
+            if continue_prompt; then
+                return 0
+            else
+                printf "Exiting...\n"
+                exit 34
+            fi
+        fi
+    else
+        return 1
+    fi
+}
+
 export -f install_software
 export -f is_sha256
+export -f is_rhel
